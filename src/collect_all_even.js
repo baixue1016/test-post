57fb197d1615dd6c674440a764cb26f2f978e2e8
baixue1016 <1773158403@qq.com>
2016-05-30 17:18:43 +0800

    commit

---
+++ src/collect_all_even.js
@@ -0,0 +1,30 @@
+
+function collect_same_elements(collection_a, object_b) {
+    var d = [];
+    var c = [];
+
+    for(var j=0;j<collection_a.length;j++){
+        for(var key in collection_a[j]){
+            var m = collection_a[j][key];
+            c.push(m);
+        }
+    }
+
+    for(var i in object_b){
+        var n = object_b[i];
+    }
+
+    for(var k = 0; k < c.length; k++){
+        for(var g = 0; g < n.length; g++){
+            if(c[k] == n[g] ){
+                d.push( n[g]);
+                break;
+            }
+        }
+    }
+
+    return d;
+
+}
+
+module.exports = collect_same_elements;
